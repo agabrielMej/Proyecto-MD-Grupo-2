5e816d7e7e5f51d15a31300a7bf4a08da6e772c5
Belen Monterroso <belenmonterroso@gmail.com>
2026-02-15 09:44:58 -0600

    data

--- data/raw/Transform/exportar_sav.py
+++ data/raw/Transform/exportar_sav.py
@@ -2,12 +2,12 @@ import pyreadstat
 import pandas as pd
 
 # el spss nombre 
-df, meta = pyreadstat.read_sav("data/raw/2022.sav")
+df, meta = pyreadstat.read_sav("data/raw/2022evaluaciones.sav")
 
 
 print("Filas:", df.shape[0])
 print("Columnas:", df.shape[1])
 
 # SPSS a csv para usar en python 
-df.to_csv("data/raw/Transform/defunciones_2022.csv", index=False, encoding="utf-8-sig")
+df.to_csv("data/raw/Transform/evaluaciones_2022.csv", index=False, encoding="utf-8-sig")
 print("arrchivo creado ")
